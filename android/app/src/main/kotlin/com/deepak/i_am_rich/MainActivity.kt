512420583f8689a7b2167770b832539e28a2ad6a
deepak <Deepakjangra605@gmail.com>
2026-06-09 05:46:27 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/deepak/i_am_rich/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.deepak.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// The main entry point of flutter apps.
+void main() {
+  runApp(
+    MaterialApp(home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Center(child: Image(image: NetworkImage('https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg'))),
+      appBar: AppBar(
+        title: Text('I Am Rich', style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
